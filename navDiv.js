87df5351fbb0c153e86bb987879f8dc5c460abff
Martin Brown <35014201+martingit9@users.noreply.github.com>
2020-07-13 15:56:32 +0100

    Made the navbar a component

---
+++ navDiv.js
@@ -0,0 +1,11 @@
+var navItems = `<div class="nav__container">
+				<ul class="menu">
+					<li class="logo"><a href="/Portfolio-2020">Martin Brown</a></li>
+					<li class="item"><a href="/Portfolio-2020">Home</a></li>
+					<li class="item"><a href="#">About</a></li>
+				</ul>
+			</div>`;
+
+
+var nav = document.getElementById("nav");
+nav.innerHTML = navItems;
